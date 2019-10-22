25f4406cd0389e63d2658dd4b269c2065a9992d1
david-seo-dev <45448504+david-seo-dev@users.noreply.github.com>
2019-10-23 09:00:30 +1100

    Created test.go
    This will do basic tests on the go endpoint api to see if a response comes through and is not a null body.

---
+++ main_test.go
@@ -0,0 +1,9 @@
+func TestIndex(t *testing.T) {
+    req, _ := http.NewRequest("GET", "/", nil)
+    response := executeRequest(req)
+    checkResponseCode(t, http.StatusOK, response.Code)
+    
+    if body := response.Body.String(); body != "[]" {
+        t.Errorf("Expected an empty array. Got %s", body)
+    }
+}
