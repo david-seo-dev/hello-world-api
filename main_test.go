788efd70637deed2114f1e2a5f81840344b187a6
david-seo-dev <45448504+david-seo-dev@users.noreply.github.com>
2019-10-23 09:11:40 +1100

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -6,6 +6,19 @@ import (
     "net/http/httptest"
 )
 
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+    rr := httptest.NewRecorder()
+    a.Router.ServeHTTP(rr, req)
+
+    return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+    if expected != actual {
+        t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+    }
+}
+
 func TestIndex(t *testing.T) {
     req, _ := http.NewRequest("GET", "/", nil)
     response := executeRequest(req)
